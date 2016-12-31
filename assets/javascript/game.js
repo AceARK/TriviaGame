558822b2b3952da5ae8cb11571d8004062fdbdc3
Ace <Ace>
2016-12-31 02:25:45 -0500

    Generating random index.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,15 @@
 // Question class
 class TriviaQuestion {
 	constructor(data) {
+		console.log("Here now and received data - " + data);
 		this.question = data.question;
-		this.rightAnswer = data.correct_answer;
-		this.choices = data.incorrect_answers;
+		this.correctAnswer = data.correct_answer;
+		this.incorrectAnswers = data.incorrect_answers;
 	}
 
-	validate() {}
+	validate() {
 
-		// return (userChoice === this.rightAnswer);
+		return (userChoice == this.rightAnswer);
 		//var result = "";
 		// if(userChoice === rightAnswer){
 		// 	wins++;
@@ -20,28 +21,30 @@ class TriviaQuestion {
 		// 	result = "wrong";
 		// }
 		//game.displayResult(result);
-		// timer.stopCountdown(); /////////// Don't know if this can access timer object created in beginGame function of game object.
-	
+		// timer.stopCountdown(); /////////// Don't know if this can access timer object created in displayQuestion function of game object.
+	}
 	
 }
 
+let countdownInterval;
 // Timer class
 class Timer {
+
 	constructor(time){
 		this.time = time;
 	}
 
 	startCountdown() {
-		var countdownInterval = setInterval(decrementTimer,1000);
+		this.countdownInterval = setInterval(decrementTimer,1000);
 	}
 
 	decrementTimer() {
-		number--;
-		$("#timeLeft").html = number;
+		time--;
+		$("#timeLeft").html = time;
 	}
 
 	stopCountdown() {
-		clearInterval(countdownInterval);
+		clearInterval(this.countdownInterval);
 	}
 }
 
@@ -54,6 +57,7 @@ var game = {
 	wrongAnswers: 0,
 	unanswered: 0,
 	questionsAndAnswersArray: [],
+	arrayOfUsedIndices : [],
 
 	getQuestions : function getQuestions()  {
 		var queryURL = "https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple";
@@ -62,67 +66,75 @@ var game = {
 			url: queryURL,
 			method: "GET"
 		}).done(function(data){
-			questionsAndAnswersArray = data.results;
-			// console.log("Array of questions - " + questionsAndAnswersArray);
+			this.questionsAndAnswersArray = data.results;
+			console.log("Array of questions - " + this.questionsAndAnswersArray[4].question);
+			console.log("Array of questions - " + this.questionsAndAnswersArray);
 		});
 	},
 
-	beginGame : function beginGame() {
-		if(questionCount == 10) {
-			game.endGame();
+	displayQuestion : function displayQuestion() {
+		if(this.questionCount == 10) {
+			this.endGame();
 		}
 		else{
-			var randomQuestionIndex = Math.floor(Math.random()*questionsAndAnswersArray.length);
-			var currentQuestion = new TriviaQuestion(questionsAndAnswersArray);// stuff gotten from trivia question api );
-			// create an object of TriviaQuestion by passing the values obtained
-			// call display question method
+			var randomQuestionIndex = Math.floor(Math.random()*10);
+			console.log("Random index - " + randomQuestionIndex);
+			while(this.arrayOfUsedIndices.indexOf(randomQuestionIndex) != -1) {
+				randomQuestionIndex = Math.floor(Math.random()*10);
+				console.log("Duplicate hence random index - " + randomQuestionIndex);
+			}
+			this.arrayOfUsedIndices.push(randomQuestionIndex);
+			console.log("Used indices array - " + this.arrayOfUsedIndices);
+			console.log("question array - " + this.questionsAndAnswersArray);
+			console.log("current question json - " + this.questionsAndAnswersArray[randomQuestionIndex]);
+			console.log("current question - " + this.questionsAndAnswersArray[randomQuestionIndex].question);
+
+			var currentQuestion = new TriviaQuestion(this.questionsAndAnswersArray[randomQuestionIndex]);
+			console.log("created currentQuestion - " + this.currentQuestion);
+
+			$("#triviaQuestion").html(currentQuestion.question);
 
 			// creating new Timer object and starting countdown
 			var timer = new Timer(15);
 			timer.startCountdown();
-			questionCount ++;
+			this.questionCount ++;
+			console.log("Questions over - " + this.questionCount);
 		}
 	},
 
-	getGiphyImage : function getGiphyImage(questionObject) {
-		var giphyImage = 0;// get image from giphy site using ajax
-		// get a word from questionObject
-		// use ajax call to query word and get image
-		//???????document.getElementByClassName("image").html("<img src='" + giphyImage + "'>"); ////// Not sure if this is done here or game.displayResult function
-	},
+	// getGiphyImage : function getGiphyImage(questionObject) {
+	// 	var giphyImage = 0;// get image from giphy site using ajax
+	// 	// get a word from questionObject
+	// 	// use ajax call to query word and get image
+	// 	//???????document.getElementByClassName("image").html("<img src='" + giphyImage + "'>"); ////// Not sure if this is done here or game.displayResult function
+	// },
 
 	displayResult : function displayResult(result) {
 		$(".displayResult").show();
-		switch(result) {
-		    case "win":
-		       $("#message").html("You are Correct!!");
-		       break;
-		    case "loss":
-		       $("#message").html("Wrong Answer!");
-		       break;
-		    default:     
-		}
 
-		$("#rightAnswer").html(currentQuestion.rightAnswer);
+		result ? $("#message").html("You are Correct!!!") : $("#message").html("Wrong Answer!");     
+		 
+		$("#rightAnswer").html(currentQuestion.correctAnswer);
 	 	//??????? $("#image").html(giphyImage);//image received by querying giphy db
-	    $(".image").html("<img src='" + giphyImage + "'>"); //////???????
+	    // $(".image").html("<img src='" + giphyImage + "'>"); //////???????
 	    setTimeOut($(".displayResult").hide(),4500);
+	    this.displayQuestion();
 	},
 
 	endGame : function endGame() {
-		$("#correctAnswers").html = game.rightAnswers;
-		$("#wrongAnswers").html = game.wrongAnswers;
-		$("#unanswered").html = game.unanswered;
+		$("#correctAnswers").html = this.rightAnswers;
+		$("#wrongAnswers").html = this.wrongAnswers;
+		$("#unanswered").html = this.unanswered;
 		$(".displayStats").show();
 	},
 
 	restartGame : function restartGame() {
-		questionArray = [];
-		questionCount = 0;
-		rightAnswers = 0;
-		wrongAnswers = 0;
-		unanswered = 0;
-		game.beginGame();
+		this.questionArray = [];
+		this.questionCount = 0;
+		this.rightAnswers = 0;
+		this.wrongAnswers = 0;
+		this.unanswered = 0;
+		this.game.displayQuestion();
 		$(".displayStats").hide();
 	}
 
@@ -130,20 +142,17 @@ var game = {
 
 // program begins
 $(document).ready(function(event) { 
-
+	ajaxCall();
+	
   	$("#start").on("click",function(){
   		console.log("Start button clicked");
-  		$.when(game.getQuestions()).done(function(){
-  			console.log("ajax call ends");
-  			game.beginGame();
-  		}).fail(function(){
-  			console.log("Couldn't fetch data.");
-  		});
+  		game.displayQuestion();
   	});
 
   	$(".choice").on("click", function(){
-  		timer.stopCountdown(); /////////// Don't know how to access timer object, created in beginGame function, here.
-  		// currentQuestion.validate(choiceClickedByUser);
+  		timer.stopCountdown(); /////////// Don't know how to access timer object, created in displayQuestion function, here.
+  		var isRightChoice = currentQuestion.validate(choiceClickedByUser);
+  		game.displayResult(isRightChoice);
   		game.getGiphyImage(currentQuestion.queryWord);
   	});
 
@@ -153,3 +162,10 @@ $(document).ready(function(event) {
 
 });
 
+function ajaxCall() {
+	$.when(game.getQuestions()).done(function(){
+		console.log("Done");
+	}).fail(function(){
+		console.log("Couldn't fetch data.");
+	});
+}
