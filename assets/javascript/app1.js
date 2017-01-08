e6adcf50c95d55b824f700411241df9e506b7689
Ace <Ace>
2017-01-08 01:52:35 -0500

    Adding functionality for category and difficulty selection; completed logic

--- assets/javascript/app1.js
+++ assets/javascript/app1.js
@@ -10,11 +10,14 @@ var game = {
 	unanswered: 0,
 	questionsAndAnswersArray: [],
 	arrayOfUsedIndices: [],
+	category: "",
+	difficulty: "",
 	countDownStarted: false,
 	countDownEnd: false,
 
-	getQuestions : function getQuestions()  {
-		var queryURL = "https://opentdb.com/api.php?amount=10&category=17&difficulty=medium&type=multiple";
+	// ajax call to opentdb to get questions on category specified with difficulty specified
+	startTrivia : function startTrivia()  {
+		var queryURL = "https://opentdb.com/api.php?amount=10&category="+game.category+"&difficulty="+game.difficulty+"&type=multiple";
 
 		return $.ajax({
 			url: queryURL,
@@ -23,9 +26,12 @@ var game = {
 			game.questionsAndAnswersArray = data.results;
 			console.log("Array of questions - " + game.questionsAndAnswersArray[4].question);
 			console.log("Array of questions - " + game.questionsAndAnswersArray);
+			// calling function to display questions
+			game.displayQuestions();
 		});
 	},
 
+	// function to display questions and choices to triviaQuestions div
 	displayQuestions : function displayQuestions() {
 		for(var i=0; i<game.questionsAndAnswersArray.length; i++) {
 			var currentQuestion = game.questionsAndAnswersArray[i];
@@ -43,83 +49,97 @@ var game = {
 				newChoice.css({'width' : '20px' , 'height' : '20px'});
 				if(k === correctAnswerIndex) {
 					choiceArray.push(currentQuestion.correct_answer);
-					newChoice.attr("data-answer", "correct");
+					newChoice.data("answer", "correct");
 				}
 				else {
 					choiceArray.push(currentQuestion.incorrect_answers[j++]);
-					newChoice.attr("data-answer", "wrong");
+					newChoice.data("answer", "wrong");
 				}
 				questionDiv.append(newChoice);
 				questionDiv.append(choiceArray[k] + "</label>");
 			}
 			$("#triviaQuestions").append(questionDiv);
-			$("#triviaQuestions").append("<br><br>");;
 		}
 
 		console.log("choice array " + choiceArray);
 		console.log("correct answer is at position " + (correctAnswerIndex+1));
+		var submitButton = $('<button class="btn btn-lg btn-info" id="submitAnswers">Submit</button>');
+		$("#triviaQuestions").append(submitButton);
+
+		$("#submitAnswers").on("click", function(){
+
+	  		console.log("submit button clicked");
+	  		game.stopCountdown();
+	  		console.log("countdown stopped. calling evaluate results");
+	  		game.evaluateResults();
+	  		$("#countDownComplete").hide();
+	  	});
 
 		game.startCountdown();
+		$("#timeLeft").show();
 	},
 
+	// timer functions
 	startCountdown : function startCountdown() {
-		if(!game.countDownEnd && !game.countDownStarted){
-			game.countdownInterval = setInterval(game.decrementTimer,1000);
-		}
+		// if(!game.countDownEnd && !game.countDownStarted){
+			countdownInterval = setInterval(game.decrementTimer,1000);
+		// }
 		game.countDownStarted = true;
 	},
-
+	
 	decrementTimer : function decrementTimer() {
-		if(game.time>0){
-			game.time--;
-		}
 
 		$("#timeLeft").html(game.time);
 		console.log("counting down - " + game.time);
+
+		if(game.time>0){
+			game.time--;
+		}
 
 		if(game.time === 0) {
 			game.countDownEnd = true;
 			game.stopCountdown();
 			game.evaluateResults();
-			game.displayResults();
+			game.endRound();
 		}
 	},
 
 	stopCountdown : function stopCountdown() {
 		game.countDownStarted = false;
-		clearInterval(game.countdownInterval);
+		game.countDownEnd = true;
+		console.log("entered stopcountdown");
+		clearInterval(countdownInterval);
+		console.log("cleared countdownInterval");
 	},
 
+	// checking if selected options are correct or wrong
 	evaluateResults : function evaluateResults() {
 
 		$('.questionAndAnswerDiv').each(function(){
 			var checkedInput = $(this).find('input[type="radio"]:checked');
-		  if(checkedInput.length > 0)
-		    {
-		       console.log(checkedInput.data('answer'));
-		    }  
-		});
+			console.dir(checkedInput);
 
+			switch(checkedInput.data('answer')) {
+				case "correct":
+					game.rightAnswers++;
+					break;
 
-		// for(var i=0; i<game.questionsAndAnswersArray.length; i++) {
-		// 	var option = "options"+i;
-		// 	var selectedOption = ".options[name="+option+"]";
-		// 	$(selectedOption).each(function() {
-		// 		if($("input :checked").attributes.data-answer.nodeValue === "correct") { 
-		//   			game.rightAnswers++;
-		//   			console.log("rightAnswer count " + game.rightAnswers);
-		//   		}
-		//   		else {
-		//   			game.wrongAnswers++;
-		//   			console.log("wrongAnswer count " + game.wrongAnswers);
-		//   		}
-		// 	})
-		// }
-		
+				case "wrong":
+					game.wrongAnswers++;
+					break;
+
+				default:
+					game.unanswered++;
+					break;
+			}
+
+			game.endRound();
+		});		
 	},
 
-	displayResults : function displayResults() {
-
+	// 
+	endRound : function endRound() {
+		$("#timeLeft").hide();
 		$("#correctAnswers").html(game.rightAnswers);
 		$("#wrongAnswers").html(game.wrongAnswers);
 
@@ -129,48 +149,65 @@ var game = {
 		if(game.time === 0){
 			$("#countDownComplete").show();
 		}	
+		$("#submitAnswers").attr('disabled', true);
 	 	
-	    // setTimeout(function(){$(".displayResults").hide()},4500);
+	    setTimeout(function(){
+	    	$(".displayResults").hide();
+	    	game.restartGame();
+	    },4500);
 	},
 
 	restartGame : function restartGame() {
-		this.questionArray = [];
+		this.time = 10;
+		// clearInterval(countdownInterval);
+		this.questionsAndAnswersArray = [];
+		this.arrayOfUsedIndices = [];
 		this.questionCount = 0;
 		this.rightAnswers = 0;
 		this.wrongAnswers = 0;
 		this.unanswered = 0;
 		this.countDownStarted = false;
 		this.countDownEnd = false
-		this.game.displayQuestion();
-		$(".displayResults").hide();
+		$("#timeLeft").hide();
+		$(".div>").hide();
+		$("#start").attr('disabled',true);
+		$(".categoryDropdown:first-child").text("Category");
+		$(".difficultyDropdown:first-child").text("Difficulty");
+		$("#triviaQuestions").empty();
 	}
 
 };
 
+var categorySelected = false;
+	var difficultySelected = false;
+
 // program begins
 $(document).ready(function(event) { 
 	$(".displayResults").hide();
-	ajaxCall();
-	
-  	$("#start").on("click",function(){
-  		console.log("Start button clicked");
-  		game.displayQuestions();
-  	});
+	$("#start").attr('disabled',true);
 
-  	// $(".options").on("click", function(){
-	   		
-   // 		if($("input: checked").attributes.data-answer.nodeValue === "correct") { ////////// this seems to not be working
-  	// 		game.rightAnswers++;
-  	// 		console.log("rightAnswer count " + game.rightAnswers);
-  	// 	}
-  	// 	else {
-  	// 		game.wrongAnswers++;
-  	// 		console.log("wrongAnswer count " + game.wrongAnswers);
-  	// 	}
-   		
-  		
-  	// });
-  
+	$(".category").on("click",function(){
+		game.category = $(this).data("category");
+		$(".categoryDropdown:first-child").text($(this).text());
+		console.log(game.category);
+		categorySelected = true;
+		console.log(categorySelected);
+		enableStartButton();
+	});
+
+	$(".difficulty").on("click",function(){
+		game.difficulty = $(this).data("difficulty");
+		$(".difficultyDropdown:first-child").text($(this).text());
+		console.log(game.difficulty);
+		difficultySelected = true;
+		console.log(difficultySelected);
+		enableStartButton();
+	});
+
+	$("#start").on("click",function(){
+  		console.log("Start button clicked");
+  		game.startTrivia();
+	});
 
   	$("#restartTrivia").on("click", function(){
   		game.restartGame();
@@ -178,10 +215,10 @@ $(document).ready(function(event) {
 
 });
 
-function ajaxCall() {
-	$.when(game.getQuestions()).done(function(){
-		console.log("Done");
-	}).fail(function(){
-		console.log("Couldn't fetch data.");
-	});
+function enableStartButton() {
+	console.log("category selected - "+categorySelected + ". difficulty selected - "+difficultySelected+".");
+	if(categorySelected && difficultySelected) {
+		console.log("entering enableSTart condition");
+		$("#start").attr('disabled',false);
+	}
 }
